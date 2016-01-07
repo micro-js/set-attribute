11f569fec18bd422cc412f2fc56e675b90b89cc9
Andrew Shaffer <andrew@weo.io>
2016-01-07 13:01:34 -0800

    add svg test

--- test/index.js
+++ test/index.js
@@ -15,5 +15,16 @@ test('should work', function (t) {
   t.equal(div.getAttribute('test'), null)
   setAttribute(div, 'test', 'val')
   t.equal(div.getAttribute('test'), 'val')
+
+  t.end()
+})
+
+test('should work with svg', function (t) {
+  var svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+
+  t.equal(svg.getAttribute('xlink:href'), null)
+  setAttribute(svg, 'xlink:href', 'test')
+  t.equal(svg.getAttribute('xlink:href'), 'test')
+
   t.end()
 })
